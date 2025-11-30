1764e20e559f31cedb9862a9096003097d6183fa
stringkey <stringkey@wxs.nl>
2025-11-30 22:46:07 +0100

    Add return type to getConfigTreeBuilder method

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('admin_lte');
         /** @var ArrayNodeDefinition $rootNode */
